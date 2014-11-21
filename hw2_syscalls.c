81ed76c0c9b6e9bf3525b8ae0e383fad3874dc6b
shadows89 <shadow89@t2.technion.ac.il>
2014-11-21 18:31:19 +0200

    HW2 initial upload

---
+++ hw2_syscalls.c
@@ -0,0 +1,10 @@
+
+
+
+int sys_lshort_query_remaining_time(int pid){
+	
+}
+
+int sys_lshort_query_overdue_time(int pid){
+	
+}
---
+++ hw2_syscalls.h
@@ -0,0 +1,47 @@
+#ifndef __LINUX_HW2_SYSCALLS_H
+#define __LINUX_HW2_SYSCALLS_H
+
+#include <errno.h>
+#include <sched.h>
+
+int lshort_query_remaining_time(int pid){
+	int res;
+	__asm__ 
+	(
+ 		"int $0x80;"
+ 		: "=a" (res) 
+ 		: "0" (243) ,"b" (pid)
+ 		: "memory"
+	);
+	if(res == -EINVAL){
+		errno = EINVAL;
+		return -1;
+	}
+	if(res == -ESRCH){
+		errno = ESRCH;
+		return -1;
+	}
+	return (int) res;
+}
+
+int lshort_query_overdue_time(int pid){
+	int res;
+	__asm__ 
+	(
+ 		"int $0x80;"
+ 		: "=a" (res) 
+ 		: "0" (244) ,"b" (pid)
+ 		: "memory"
+	);
+	if(res == -EINVAL){
+		errno = EINVAL;
+		return -1;
+	}
+	if(res == -ESRCH){
+		errno = ESRCH;
+		return -1;
+	}
+	return (int) res;
+}
+
+#endif
