8234a46c32f4922e3c1d3ac5f1425ab9df1d14f6
Sergey <ser.buh2@gmail.com>
2014-12-02 22:39:06 +0100

    copy.bash update names inside the script

--- copy.bash
+++ copy.bash
@@ -1,26 +1,26 @@
 echo Copying...
 
-cp kernel/kernel/sched.c /usr/src/linux-2.4.18-14custom/kernel
+cp linux/kernel/sched.c /usr/src/linux-2.4.18-14custom/kernel
 echo -n "1 "
 
-cp kernel/kernel/fork.c /usr/src/linux-2.4.18-14custom/kernel
+cp linux/kernel/fork.c /usr/src/linux-2.4.18-14custom/kernel
 echo -n "2 "
 
-cp kernel/kernel/exit.c /usr/src/linux-2.4.18-14custom/kernel
+cp linux/kernel/exit.c /usr/src/linux-2.4.18-14custom/kernel
 echo -n "3 "
 
-cp kernel/kernel/hw2_syscalls.c /usr/src/linux-2.4.18-14custom/kernel
+cp linux/kernel/hw2_syscalls.c /usr/src/linux-2.4.18-14custom/kernel
 echo -n "4 "
 
-cp kernel/kernel/Makefile /usr/src/linux-2.4.18-14custom/kernel
+cp linux/kernel/Makefile /usr/src/linux-2.4.18-14custom/kernel
 echo -n "5 "
 
-cp kernel/include/linux/sched.h /usr/src/linux-2.4.18-14custom/include/linux
+cp linux/include/linux/sched.h /usr/src/linux-2.4.18-14custom/include/linux
 echo -n "6 "
 
-cp kernel/include/asm-i386/unistd.h /usr/src/linux-2.4.18-14custom/include/asm-i386
+cp linux/include/asm-i386/unistd.h /usr/src/linux-2.4.18-14custom/include/asm-i386
 echo -n "7 "
 
-cp kernel/arch/i386/kernel/entry.S /usr/src/linux-2.4.18-14custom/arch/i386/kernel
+cp linux/arch/i386/kernel/entry.S /usr/src/linux-2.4.18-14custom/arch/i386/kernel
 echo "8 "
 echo done
