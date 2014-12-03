310e98b25a7b20066229c3154a1fde3930057e0e
Sergey <ser.buh2@gmail.com>
2014-12-03 12:17:22 +0100

    first version of hw2_tests.c

--- hw2_test.c
+++ hw2_test.c
@@ -19,40 +19,44 @@ int main(int argc, char** argv){
 	int pid;
 	int i;
 	for (i = 1; i < argc; ){
-		int trial_num = strtol(argv[i++], NULL, 0);
-		int fibunchi = strtol(argv[i++], NULL, 0);
+		int requested_time = strtol(argv[i++], NULL, 0); 
+		int level = strtol(argv[i++], NULL, 0);
+		int fib_num= strtol(argv[i++], NULL, 0);
 		pid = fork();
 		if (pid==0) {
 			usleep(40000);
-			fibonaci(fibunchi);
+			fibonaci(fib_num);
 			exit(0);
 		} 
-		else if(pid < 0){
+		else if(pid<0){
 			perror("error forking");
 			exit(1);
 		}
+
 		struct sched_param param;
+
 		param.sched_priority = 0;
-		param.trial_num = trial_num;
-		int res = sched_setscheduler(pid, SCHED_MQ, &param);
-		if (res) {
-			printf("error. setsched ret= %d,errno value=%d\n",res,errno);
-		}
-		else {
-			printf("trial number=%d, fib=%d, pid=%d is_mq=%d\n", trial_num, fibunchi,pid,is_MQ(pid));
-		}
+		param.lshort_sched_param.requested_time = requested_time;
+		param.lshort_sched_param.level = level;
+
+		int res = sched_setscheduler(pid, SCHED_LSHORT, &param);
+		if (res)
+			printf("error. setsched ret= %d,errno value=%d\n", res, errno);
+		else
+			printf("level=%d, fib_num=%d, pid=%d\n", level, fib_num, pid);
 	}
+	// int remain_time = lshort_query_remaining_time(pid);
 	while (wait() != -1);
-	struct switch_info info[150];
-	int len=get_scheduling_statistic(info);
-	if (len < 0) {
-		printf("get statistics error,ret=%d,errno=%d\n",len,errno);
-		exit (-1);
-	}
-	printf("prev_p-next_p---prev_policy---next_policy---time---reason\n");
-	for (i = 0; i < len; i++)
-		printf("%04d     %04d    %8s   %8s       %d %s\n",info[i].previous_pid, info[i].next_pid,
-			policies[info[i].previous_policy],policies[info[i].next_policy],
-			info[i].time,switch_reason[info[i].reason]);
+	// struct switch_info info[150];
+	// int len = get_scheduling_statistic(info);
+	// if (len<0) {
+	// 	printf("get statistics error,ret=%d,errno=%d\n",len,errno);
+	// 	exit (-1);
+	// }
+	// printf("prev_p-next_p---prev_policy---next_policy---time---reason\n");
+	// for (i=0;i<len ;i++)
+	// 	printf("%04d     %04d    %8s   %8s       %d %s\n",info[i].previous_pid, info[i].next_pid,
+	// 		policies[info[i].previous_policy],policies[info[i].next_policy],
+	// 		info[i].time,reasons[info[i].reason]);
 	return 0;
 }
